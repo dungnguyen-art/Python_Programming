10cfe81970332bd1816bf314b3b35fc2c9746a63
NGUYEN TIEN DUNG <dungtpa2@gmail.com>
2021-09-30 19:03:51 +0700

    Wrong Answer

--- PY03004.py
+++ PY03004.py
@@ -2,24 +2,25 @@ import operator
 from string import punctuation
 n = int(input())
 res = []
-sp = set(punctuation)
+sp = set(punctuation)   # tap ki tu dac biet
 while n > 0:
     n -= 1
     s = input()
     for i in range(len(s)):
         if s[i] in sp:
-            s = s.replace(s[i], " ")
+            s = s.replace(s[i], " ")    # thay doi ki tu dac biet bang " "
     s = s.lower()
     s = s.split()
     res.append(s)
-l = [item for sublist in res for item in sublist]
-l.sort()
+
+l = [item for sublist in res for item in sublist]   # convert nested list to list
+l.sort()        # sap xep theo thu tu tu dien
 res_dct = {}
 
 for i in l:
     if i not in res_dct:
         res_dct[i] = l.count(i)
-# sap xep dictionary theo value giam dan
-sorted_d = dict(sorted(res_dct.items(), key=operator.itemgetter(1), reverse=True))
+
+sorted_d = dict(sorted(res_dct.items(), key=operator.itemgetter(1), reverse=True)) # sap xep dictionary theo value giam dan
 for i in sorted_d:
     print(i,sorted_d[i])
