f0b471bcf0652305b410215861eeb1194e7905aa
NGUYEN TIEN DUNG <dungtpa2@gmail.com>
2021-10-02 06:55:08 +0700

    AC
    ordinal

--- PY01022.py
+++ PY01022.py
@@ -1,19 +1,19 @@
-# from sys import stdin, stdout
-# from time import perf_counter
-# st = input()
-# if st[0] == "-":
-#     st = st[1:]
-# cnt = 0
-# if len(st) == 1:
-#     cnt = 0
-# else:
-#     while len(st) > 1:
-#         sm = 0
-#         for i in range(len(st)):
-#             sm += int(st[i])
-#         st = f"{sm}"
-#         cnt += 1
-#     # t1_stop = perf_counter()
-# print(cnt)
-print(ord("-"))
-print(ord("0"))
+from sys import stdin, stdout
+st = input()
+flag = 1
+
+cnt = 0
+if len(st) == 1:
+    cnt = 1
+else:
+    while len(st) > 1:
+        sm = 0
+        for i in range(len(st)):
+            if st[i] == '-':
+                sm += ord('-')-ord('0')
+            else:
+                sm += int(st[i])
+        st = f"{sm}"
+        cnt += 1
+print(cnt)
+
