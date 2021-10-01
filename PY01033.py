94af91d6582f24f382136855880b12d8e9911ca8
NGUYEN TIEN DUNG <dungtpa2@gmail.com>
2021-10-02 06:56:04 +0700

    AC
    Naive approach algorithm

---
+++ PY01033.py
@@ -0,0 +1,9 @@
+import math
+
+s = input().split()
+a, b = int(s[0]), int(s[1])
+for i in range(a, b - 1):
+    for j in range(i + 1, b):
+        for k in range(j + 1, b+1):
+            if math.gcd(i, j) == 1 and math.gcd(j, k) == 1 and math.gcd(k, i) == 1:
+                print("("+str(i)+', '+str(j)+', '+str(k)+")")
